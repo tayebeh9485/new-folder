13d8928679f8c2e622620ad5deb911e7a9a02b83
tayebeh <tayebeh.karimi1404@gmail.com>
2025-12-29 04:36:07 -0800

    Update tk1.py
    قطعه کدی جهت ساختن فولدر جدید روی دسکتاپ

--- tk1.py
+++ tk1.py
@@ -4,7 +4,8 @@ import os
 
 new_folder_path = os.path.join(
     os.path.expanduser("~"), "desktop", "folder")
-
+# به جای کلمه ی فولدر (folder)در خط بالا  اسم پوشه ی مورد نظرتون رو بنویسید 
 # ساختن پوشه
 os.mkdir(new_folder_path)
 print("creat new folder", new_folder_path)
+
